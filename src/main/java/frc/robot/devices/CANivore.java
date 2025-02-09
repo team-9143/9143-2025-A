38dbd71bc2a758b643dfe47549dafdbbd71be70f
Armor Robotics 9143 <robotics@riordanhs.org>
2025-02-08 20:42:35 -0800

    feat: update CANivore for use on swerve devices

--- src/main/java/frc/robot/devices/CANivore.java
+++ src/main/java/frc/robot/devices/CANivore.java
@@ -1,47 +1,11 @@
-/*
 package frc.robot.devices;
 
-import java.io.ObjectInputFilter.Config;
-
-import com.ctre.phoenix.sensors.*;
-
-import com.ctre.phoenix.platform.can.*;
-import com.ctre.phoenix6.*;
-import com.ctre.phoenix6.configs.*;
-
 public class CANivore {
-	private CANivore sensorCANivore;
-	private PigeonIMU gyro;
-
-	public CANivore() {
-		//Initialize CANivore for non-drivetrain devices
-		sensorCANivore = new CANivore("SensorBus"); //SensorBus is an example name we can give the auxiliary CAN bus
-		
-		//Example: Initialize non-drivetrain devices on the CANivore
-		gyro = new PigeonIMU(2); //CAN id 10 on Sensor bus
-	}
-
-	private String getName() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'getName'");
-	}
-	
-	public CANivore(String string) {
-		//TODO Auto-generated constructor stub
-	}
-
-	public void robotInit() {
-		//example: Configure sensors if needed
-		PigeonIMU gyro;((CANivore) getConfigurator()).applyDefaultConfiguration();
-	}
-
-	private Object getConfigurator() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'getConfigurator'");
-	}
-
-	private Object applyDefaultConfiguration() {
-		return gyro;
-	}
+    private final String CANIVORE_BUS_NAME = "Swerve";  // Name of the CANivore bus
+    
+    public CANivore() {}
+    
+    public String getBusName() {
+        return CANIVORE_BUS_NAME;
+    }
 }
-*/
